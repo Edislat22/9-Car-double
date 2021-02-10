9e91174601b8d08d3fbdd15692935dbcabce917c
Eduardas <latoanas@gmail.com>
2021-02-10 13:54:44 +0200

    9-Car-double #1

---
+++ js/components/Car.js
@@ -0,0 +1,40 @@
+class Car {
+   constructor(tank, consumption) {
+      this.engine = false;
+      this.mileage = 0;
+      this.tank = tank;                 // litres
+      this.consumption = consumption;   // litres/100km
+   }
+
+   turnOn() {
+      this.engine = true;
+      console.log('Varyklis ijungtas!');
+   }
+   turnOff() {
+      this.engine = false;
+      console.log('Varyklis isjungtas!');
+   }
+
+   drive(distance) {
+      if (!this.engine) {
+         console.log('Norint vazioti - ijunk varykli!');
+         return false;
+      }
+
+      const maxDistanceAvailable = this.tank / this.consumption * 100;
+      if (maxDistanceAvailable >= distance) {
+          this.mileage += distance;
+          this.tank -= distance * this.consumption / 100;
+          console.log('Nuvaziavo: ', distance);
+      } else {
+         this.mileage += this.maxDistanceAvailable;
+         console.log('Nuvaziavo: ', maxDistanceAvailable);
+         this.tank = 0;
+         this.turnOff();
+         console.log(`Bake per mazai norimam atstumui: nuvaziavo 
+         ${this.maxDistanceAvailable} ir liko nenuvaziuoti ${distance - maxDistanceAvailable}`);
+      }
+   }
+}
+   
+export { Car }
